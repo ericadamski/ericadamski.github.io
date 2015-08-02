f555d826eead11191a88a2d40559e1145e586d3f
Eric Adamski <er.adamski@gmail.com>
2015-08-02 10:41:54 -0400

    Update cards, center icons, added skrollr to destop version

--- js/shared/parallax.js
+++ js/shared/parallax.js
@@ -1,4 +1,11 @@
 $(function() {
+
+  if(!(/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera)){
+    skrollr.init({
+      forceHeight: false
+    });
+  }
+
   var collapseList = [
     "#work",
     "#education",
