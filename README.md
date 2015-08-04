e4718b9b486a3509e47d7c87c0d3822e777e2e3b
Eric Adamski <er.adamski@gmail.com>
2015-08-04 17:56:47 -0400

    Update, added README

---
+++ README.md
@@ -0,0 +1,2 @@
+# My Own Personal Web Page
+Accessible [here](http://ericadamski.com/) or [here](http://ericadamski.github.io)! :+1:
--- js/node/app.js
+++ js/node/app.js
@@ -9,17 +9,13 @@ var routes = require('./routes/index');
 
 var app = express();
 
-// view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '../../../img/e.png'));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(require('stylus').middleware(path.join(__dirname, 'style')));
+
 app.use('/css', express.static(path.join(__dirname, '..', '..', 'css')));
 app.use('/bower_components',
   express.static(path.join(__dirname, '..', '..', 'bower_components')));
@@ -31,17 +27,12 @@ app.use('/portfolio',
 
 app.use('/', routes);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
@@ -52,8 +43,6 @@ if (app.get('env') === 'development') {
   });
 }
 
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
