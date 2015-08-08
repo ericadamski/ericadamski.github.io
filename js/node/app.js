e623d98e13cc0587ef74bcee64457aa7cf184d84
Eric Adamski <er.adamski@gmail.com>
2015-08-07 23:14:52 -0400

    add site map

--- js/node/app.js
+++ js/node/app.js
@@ -16,6 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use('/', express.static(path.join(__dirname, '..', '..', 'public')));
 app.use('/css', express.static(path.join(__dirname, '..', '..', 'css')));
 app.use('/bower_components',
   express.static(path.join(__dirname, '..', '..', 'bower_components')));
