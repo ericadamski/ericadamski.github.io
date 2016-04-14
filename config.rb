57b163f3372d61a0a0a56d88d669afa3f17978c9
Eric Adamski <er.adamski@gmail.com>
2016-04-13 20:43:43 -0400

    Update: reorganize

---
+++ config.rb
@@ -0,0 +1,9 @@
+require 'compass/import-once/activate'
+
+http_path = "/"
+css_dir = "public/stylesheets"
+sass_dir = "assets/scss"
+images_dir = "public/img"
+javascripts_dir = "public/js"
+
+output_style = :compressed
